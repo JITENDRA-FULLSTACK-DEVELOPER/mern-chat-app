40f490069439d4ebf50b37acfec22a5e933893c0
Jitendra-civil <jitendrapanigrahi16@gmail.com>
2024-06-13 11:59:52 +0530

    Socket url updated

--- frontend/src/Context/SocketContext.jsx
+++ frontend/src/Context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider=({children})=>{
 
     useEffect(()=>{
         if(authUser) {
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://mern-chat-app-6b8n.onrender.com/",{
                 query:{
                     userId:authUser._id,
                 }
